0c74230279edfcf6e0bbe1fb802001ff74a07753
Rafsun Jani <1999rafsun@gmail.com>
2026-04-18 01:36:27 +0600

    Feat: added sniffer

--- wphz-ugc-carousel.php
+++ wphz-ugc-carousel.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name: WPHZ UGC Carousel
- * Plugin URI:  https:// wphelpzone.com.
+ * Plugin Name: UGC Carousels & Shoppable Videos for WooCommerce
+ * Plugin URI:  https://www.wphelpzone.com/plugins/ugc-carousels-for-woo/
  * Description: User-Generated Content video carousel with WooCommerce product attachment.
- * Version:     1.0.7
- * Author:      WPHelpZone LLC
- * Text Domain: wphz-ugc
+ * Version:     1.0.8
+ * Author:      WP Help Zone
+ * Text Domain: ugc-carousels-for-woo
  * Requires at least: 6.0
  * Requires PHP: 8.0
  * WC requires at least: 7.0
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WPHZ_UGC_VERSION', '1.0.7' );
+define( 'WPHZ_UGC_VERSION', '1.0.8' );
 define( 'WPHZ_UGC_FILE', __FILE__ );
 define( 'WPHZ_UGC_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WPHZ_UGC_URL', plugin_dir_url( __FILE__ ) );
